79503b80bddf0cfc5a7b44da0ca515cd32364004
pedrofxx2 <pedroh.fortunatos@gmail.com>
2020-04-29 11:20:05 -0300

    inittial commit

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '65ce2a7e98f2d881d2c7f32d3ef757c9',
+    expiresIn: '1d',
+  },
+};
---
+++ src/services/CreateAppointmentServer.ts
@@ -0,0 +1,42 @@
+import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+import Appointment from '../models/Appointments';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import AppError from '../errors/AppError';
+/**
+ * Recebimento das informações
+ * Tratativa de erros/excessões
+ * Aceddo ao repositorio
+ */
+
+interface Request {
+  provider_id: string;
+  date: Date;
+}
+
+class CreateAppointmentServer {
+  public async execute({ date, provider_id }: Request): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw new AppError('This appointment is already booked');
+    }
+
+    const appointment = appointmentsRepository.create({
+      provider_id,
+      date: appointmentDate,
+    });
+
+    await appointmentsRepository.save(appointment);
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentServer;
